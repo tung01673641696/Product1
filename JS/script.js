dd7a74709cf5a2e4d1388aa759f147729a4f8a47
hoàng thanh tùng <hoangthanhtung381999@gmail.com>
2021-09-24 01:09:31 +0700

    trang web hoa qua sach

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        576:{
+            items:2
+        },
+        768:{
+            items:3
+        },
+        1200:{
+            items:4
+        }
+    }
+})
+
